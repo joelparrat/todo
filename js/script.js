09e8f63baa165effdb4d9fb34f25e9a35db6e565
joelparrat <joel.parrat@gmail.com>
2019-01-07 13:45:16 +0100

    ecran connexion

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+$(																														// jquery: attente chargement dom
+	function()
+	{
+		$('.connexion').click																							// evenement click du bouton connexion
+		(
+			function()
+			{
+				if (($('[name="lgn"]').val() != "") && (($('[name="pwd"]').val() != "")))								// controle si la saisie a ete faite (oui)
+				{
+					let objJSON =																						// creation d'un objet js pour json
+					{
+						lgn: $('[name="lgn"]').val(),																	// le login (nom+valeur)
+						pwd: $('[name="pwd"]').val()																	// le password (nom+valeur)
+					};
+					
+					$.post																								// envoi json avec la methode post
+					(
+						"../php/fonction.php",																			// url de destination (php)
+						JSON.stringify(objJSON), 																		// convertie l'objet js en texte json
+						function(reponse)																				// quand le serveur repond ca va dans reponse
+						{
+							//console.log(reponse);
+							let rcv = $.parseJSON(reponse);																// conversion objet json texte en objet js (rcv)
+							//console.log(rcv.mss);
+							$('.message').html(rcv.mss);
+							if (!rcv.rtr)																				// droit en ecriture
+							{
+								$('[name="lgn"]').css("color", "black");
+								$('[name="pwd"]').css("color", "black");
+								$('.message').css("color", "green");
+								window.setTimeout(affPage, 5000);														// ajout attente pour demo
+							}
+							else if (rcv.rtr == 1)																		// droit en lecture
+							{
+								$('[name="lgn"]').css("color", "darkblue");
+								$('[name="pwd"]').css("color", "darkblue");
+								$('.message').css("color", "darkblue");
+								window.setTimeout(affPage, 5000);														// ajout attente pour demo
+							}
+							else																						// pas autorise
+							{
+								//$('#bdd') disabled
+								//$('#adm') disabled
+								$('[name="lgn"]').css("color", "red");
+								$('[name="pwd"]').css("color", "red");
+								$('.message').css("color", "darkblue");
+							}
+						}
+					);
+
+				}
+				else																									// controle si la saisie a ete faite (non)
+				{
+					$('.message').css("color", "red");																	// positionne la couleur du message d'erreur (rouge)
+					if (($('[name="lgn"]').val() == "") && ($('[name="pwd"]').val() == ""))								// on n'a ni le login ni le password
+						$('.message').html("Veuillez vous identifier ...");												// on affiche le message d'erreur
+					else if ($('[name="lgn"]').val() == "")																// on n'a pas le login
+						$('.message').html("Entrez votre identifiant ...");												// on affiche le message d'erreur
+					else																								// on n'a pas le password
+						$('.message').html("Entrez votre mot de passe ...");											// on affiche le message d'erreur
+				}
+			}
+		)
+		
+		function affPage()
+		{
+			$(location).attr('href',"gstflm.html");
+		} 
+	}
+);
+
--- php/fonction.php
+++ php/fonction.php
@@ -1,16 +1,104 @@
 <?php
 
-$prenom = array('joel', 'corinne', 'emanuelle');
-$coordonnee = array(
-'prenom' => 'joel',
-'nom' => 'parrat')
-//echo $prenom[0];
-//echo ' ';
-//echo $coordonnee['nom'];
-for ($i=0; $i<3; $i++)
-    echo $prenom[$i].<br>;
+	class clsJSN																										// classe JSON
+	{																													// *** les proprietes champ bdd ***
+		private $clf;																									// clef
+		private $lgn;																									// login
+		private $pwd;																									// password
+		private $drt;																									// droit
+																														// *** les proprietes acces bdd ***
+		private $bdd;																									// handle de la bdd
+		private $rqt;																									// requete sql
+		private $rsl;																									// resultat en memoire de la requete
+		private $vlr;																									// valeur reel lu dans la base
+																														// *** les propriete pour repondre ***
+		public $mss;																									// message retour
+		public $rtr;																									// code retour
+																														// *** les methodes ***
+		public function __construct($lgn, $pwd)																				// constructeur de la classe json
+		{
+			$this->rtr=-1;																								// reponse: code retour par defaut
+			$this->mss="Veuillez vous identifier ...";																	// reponse: message par defaut
+			
+			$this->lgn = $lgn;																							// remplie avec la saisie (form)
+			$this->pwd = $pwd;																							// remplie avec la saisie (form)
+		}
+		
+		public function lectureBDD()																					// lecture de la bdd
+		{
+			$this->bdd = mysqli_connect('localhost', 'todo', 'abcABC123$', 'todo');										// ouverture de la base (user password table)
+			if (!$this->bdd)																							// erreur connexion bdd
+			{
+				$this->mss = mysqli_connect_error();
+				$this->rtr=-2;
+				return false;
+			}
+			
+			$this->rqt = 'select drt from usr where lgn="'.$this->lgn.'" and pwd="'.$this->pwd.'"';						// genere la requete sql dynamiquement
+			$this->rsl = mysqli_query($this->bdd, $this->rqt);															// envoie la requete au gestionnaire de bdd (mysql)
+			$this->vlr = mysqli_fetch_assoc($this->rsl);																// recupere physiquement les donnees (plus cache)
+			mysqli_close($this->bdd);																					// referme la bdd
+			
+			if ($this->rsl->num_rows == 0)																				// pas d'article correspondant dans la base
+			{
+				$this->rtr = 3;
+				$this->mss="Utilisateur inconnu ...";
+			}
+			else if ($this->rsl->num_rows == 1)																			// une seule reponse
+			{
+				$this->drt=$this->vlr['drt'];																			// sauve la valeur lue (droit)
+				if ($this->drt == 9)																					// droit en ecriture (9)
+				{
+					$this->rtr = 0;
+					$this->mss="Controle total ...";
+				}
+				else if ($this->drt == 0)																				// pas autorise (0)
+				{
+					$this->rtr = 2;
+					$this->mss="Acces inderdit ...";
+				}
+				else																									// droit en lecture (de 1 a 8)
+				{
+					$this->rtr = 1;
+					$this->mss="Acces autorise ...";
+				}
+			}
 
-foreach ($prenom as $element)
-    echo $element.<br>;
+			return true;
+		}
+		
+		public function sndRPN()																						// envoi reponse
+		{
+			echo json_encode($clsjsn);																					// renvoie au js le json convertie en texte
+		}
+/*		
+		public function setmss($mss)																					// setter du message en retour
+		{
+			$this->mss = $mss;
+		}
+		public function setrtr($rtr)																					// setter du code retour
+		{
+			$this->rtr = $rtr;
+		}
+*/
+	}
+	
+	$rcv = trim(file_get_contents("php://input"));																		// lecture du post (deja controle en js)
+	$rcvOBJ = json_decode($rcv);																						// conversion json text (rcv) en objet php (rcvOBJ)
+
+	$clsjsn = new clsJSN($rcvOBJ->lgn, $rcvOBJ->pwd);																	// creation instance classe json
+
+	if (!is_object($rcvOBJ))																							// si erreur format json --> reponse erreur + arret
+	{
+		$clsjsn->mss = "Erreur objet JSON";																				// message d'erreur
+		$clsjsn->rtr = -3;																								// code erreur
+		echo json_encode($clsjsn);																						// renvoie au js le json convertie en texte
+		return false;
+	}
+
+	$clsjsn->lectureBDD();																								// lecture des droit dans la bdd
+	//$clsjsn->sndRPN();																								// repond au js 
+	echo json_encode($clsjsn);																							// renvoie au js le json convertie en texte
+	return true;
  ?>
 
